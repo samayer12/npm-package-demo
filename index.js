13e8401ab9816572b4d4d31a6d06d17938260123
Sam Mayer <samuel.a.mayer5.ctr@army.mil>
2024-01-10 13:20:32 -0600

    Create a simple styled component

--- index.js
+++
@@ -1,5 +0,0 @@
-function printMsg(){
-  console.log("This is a message from the demo package");
-}
-
-export { printMsg }
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import {MyButtonComponent} from './component/MyButton/MyButton';
+
+function printMsg(){
+  console.log("This is a message from the demo package");
+}
+
+export { printMsg, MyButtonComponent }
