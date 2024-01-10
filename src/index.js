f190ecb3305e92596fce26ca307be4d7b3dc2b42
Sam Mayer <samuel.a.mayer5.ctr@army.mil>
2024-01-10 15:05:43 -0600

    Update export details

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
 import {ImportedComponent} from "./components/ImportedComponent/ImportedComponent";
 
-function printMsg(){
-  console.log("This is a message from the demo package");
-}
-
-export {printMsg, ImportedComponent}
+export {ImportedComponent}
